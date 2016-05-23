65c51a41043c1fc7c1cfc8529bbb83e00c297f56
ImayaDismas <imayadismas@gmail.com>
2016-05-23 23:43:16 +0300

    Edited the create profile activity and layout

--- app/src/main/java/com/example/jaykayitare/theforce/CreateProfile.java
+++ app/src/main/java/com/example/jaykayitare/theforce/CreateProfile.java
@@ -2,6 +2,7 @@ package com.example.jaykayitare.theforce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class CreateProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+        setTitle(null);
+
+        Toolbar topToolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+        topToolBar.setLogo(R.drawable.btn_navbar_back2x);
+        topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
     }
 }

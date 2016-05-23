b67d6e25c60b8b4b97d0ada47214932bd13cc335
ImayaDismas <imayadismas@gmail.com>
2016-05-23 23:57:10 +0300

    Added the title of the create profile page

--- app/src/main/java/com/example/jaykayitare/theforce/CreateProfile.java
+++ app/src/main/java/com/example/jaykayitare/theforce/CreateProfile.java
@@ -3,6 +3,9 @@ package com.example.jaykayitare.theforce;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -16,5 +19,26 @@ public class CreateProfile extends AppCompatActivity {
         setSupportActionBar(topToolBar);
         topToolBar.setLogo(R.drawable.btn_navbar_back2x);
         topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.profile_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.create_profile) {
+            Toast.makeText(CreateProfile.this, "Create Profile", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

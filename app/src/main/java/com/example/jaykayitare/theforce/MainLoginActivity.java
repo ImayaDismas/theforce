b91792a22df535cdfd8f22f8b30bb0dde048eb7c
ImayaDismas <imayadismas@gmail.com>
2016-05-23 22:11:29 +0300

    Linked the signup login and createprofile activities

--- app/src/main/java/com/example/jaykayitare/theforce/MainLoginActivity.java
+++ app/src/main/java/com/example/jaykayitare/theforce/MainLoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.jaykayitare.theforce;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -15,5 +18,25 @@ public class MainLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlogin_activity);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, CreateProfile.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/example/jaykayitare/theforce/SignUpActivity.java
+++ app/src/main/java/com/example/jaykayitare/theforce/SignUpActivity.java
@@ -1,16 +1,42 @@
 package com.example.jaykayitare.theforce;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by imaya on 5/23/16.
  */
 public class SignUpActivity extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, MainLoginActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }

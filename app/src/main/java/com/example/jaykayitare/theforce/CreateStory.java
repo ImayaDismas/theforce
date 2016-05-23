7e7c04ad39854786d9b455e9638fe83a02311333
ImayaDismas <imayadismas@gmail.com>
2016-05-24 02:20:22 +0300

    Added an add story activity and layout

--- app/src/main/java/com/example/jaykayitare/theforce/CreateStory.java
+++ app/src/main/java/com/example/jaykayitare/theforce/CreateStory.java
@@ -1,23 +1,23 @@
 package com.example.jaykayitare.theforce;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-public class CreateProfile extends AppCompatActivity {
+/**
+ * Created by imaya on 5/24/16.
+ */
+public class CreateStory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_profile);
+        setContentView(R.layout.activity_create_story);
         setTitle(null);
 
         Toolbar topToolBar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(topToolBar);
-        topToolBar.setLogo(R.drawable.btn_navbar_back2x);
+        topToolBar.setLogo(R.drawable.navbar_logo2x);
         topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
     }
 }

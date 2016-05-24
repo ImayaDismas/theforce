98cad526ec27d4a618b36d5973ba4ccfd5f2a111
ImayaDismas <imayadismas@gmail.com>
2016-05-24 03:40:11 +0300

    Added a listener to the profile layout and linked to create story

--- app/src/main/java/com/example/jaykayitare/theforce/CreateProfile.java
+++ app/src/main/java/com/example/jaykayitare/theforce/CreateProfile.java
@@ -1,13 +1,18 @@
 package com.example.jaykayitare.theforce;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class CreateProfile extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,25 @@ public class CreateProfile extends AppCompatActivity {
         setSupportActionBar(topToolBar);
         topToolBar.setLogo(R.drawable.btn_navbar_back2x);
         topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.create_profile);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, CreateStory.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/example/jaykayitare/theforce/CreateStory.java
+++ app/src/main/java/com/example/jaykayitare/theforce/CreateStory.java
@@ -3,6 +3,9 @@ package com.example.jaykayitare.theforce;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by imaya on 5/24/16.
@@ -19,5 +22,25 @@ public class CreateStory extends AppCompatActivity {
         setSupportActionBar(topToolBar);
         topToolBar.setLogo(R.drawable.navbar_logo2x);
         topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.create_story, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            Toast.makeText(CreateStory.this, "Search", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
